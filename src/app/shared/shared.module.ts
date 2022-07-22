8ca706b110e7314f9aaf02b2be5186756f297118
Ayilk <srangel.h92@gmail.com>
2022-07-22 15:03:33 -0500

    Se movio parte de la estructura de app.component al componente sidebar, con ello se importo RouterModule en el shared.module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    
   ],
   exports: [
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
